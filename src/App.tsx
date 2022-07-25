b1af689f00aee9907c2be4c59ac29559dde27129
Nikola Đuza <nikolaseap@gmail.com>
2022-07-25 10:46:02 +0200

    Add a Helpbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Picasso from "@toptal/picasso-provider";
-import { Button, Page, Container } from "@toptal/picasso";
+import { Button, Page, Container, Helpbox } from "@toptal/picasso";
 
 function App() {
   return (
@@ -15,12 +15,24 @@ function App() {
               justifyContent="center"
               direction="column"
             >
-              <Container flex bottom={2}>
-                Your application goes here
-              </Container>
-              <Container>
+              <Container bottom={2}>Your application goes here</Container>
+              <Container bottom={2}>
                 <Button>Hello world!</Button>
               </Container>
+
+              <Helpbox>
+                <Helpbox.Title>Heading Small</Helpbox.Title>
+                <Helpbox.Content>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Helpbox.Content>
+              </Helpbox>
             </Container>
           </Page.Article>
         </Page.Content>
